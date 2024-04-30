9326f921cef378627d4ba2824bcbacad9f0fcc9c
Omkar <omkarbokil22@gmail.com>
2024-05-01 01:03:33 +0530

    commit

--- JS/portfolio.js
+++
@@ -1,9 +0,0 @@
-let htmlTab = document.querySelector("#html-tab");
-
-htmlTab.addEventListener("mouseover",() => {
-     htmlTab.innerText = '<h1> HTML </h1>'
-})
-
-htmlTab.addEventListener("mouseout",() => {
-     htmlTab.innerText = 'HTML'
-})
